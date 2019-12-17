d383ae738bc776298fd2584c1f8bb51a4762f702
VigneshwaranJB <58186438+VigneshwaranJB@users.noreply.github.com>
2019-12-17 12:35:32 +0530

    Update two.py

--- two.py
+++ two.py
@@ -2,5 +2,9 @@ a=int(input())
 b=int(input())
 if a>b:
     print("'a' is greater than 'b'")
-else:
+elif b>a:
     print("'b' is greater than 'a'")
+elif a==b:
+    print("'b' and 'a' are equal numbers")
+else:
+    print("Invalid input")
